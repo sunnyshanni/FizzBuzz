e712c9093264bf1ae15e958b453538bbb8440d9f
sunnyshanni <sunnyshanni.cai@mail.utoronto.ca>
2025-09-08 14:07:53 -0400

    prints how many multiples of 3 or 5 there are below 1000

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -3,16 +3,17 @@ package lab1;
 public class Multiples {
     public static void main(String[] args) {
 
+        System.out.println(multiples(1000,3,5));
     }
 
-    public static void multiples() {
+     public static int multiples(int n, int a, int b) {
         int i = 1;
         int count = 0;
-        while (i < 1000) {
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+        while (i < n) {
+            boolean divisibleBya = i % a == 0;
+            boolean divisibleByb = i % b == 0;
 
-            if (divisibleBy3 || divisibleBy5) {
+            if (divisibleBya || divisibleByb) {
                 count++;
 
             }
@@ -20,6 +21,6 @@ public class Multiples {
             i++;
 
         }
-        System.out.println(count);
+        return count;
     }
 }
